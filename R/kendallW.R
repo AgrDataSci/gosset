1b900cc9d1b11b28c2ab4d4bc1a8b0e78f233494
David Brown <50154982+dbrownf@users.noreply.github.com>
2022-04-19 11:09:21 -0600

    Create kendallW.R

---
+++ R/kendallW.R
@@ -0,0 +1,95 @@
+#' Kendall's W (coefficient of concordance)
+#' 
+#' Compute Kendall's W, also known as coefficient of concordance among observed rankings
+#' and those predicted by the Plackett-Luce model.
+#' 
+#' It is as wrapper of the function DescTools::KendallW, adapted to compute the Kendall's W
+#' on the observed and predicted rankings.
+#'  
+#' Kendall's W values range between 0 (no agreement) to 1 (full agreement)
+#' 
+#' @author David Brown, Kauê de Sousa and Jacob van Etten
+#' @family goodness-of-fit functions
+#' @param x an object of class \code{pltree}
+#' @param newdata data for predictions
+#' @param ... additional arguments passed to methods
+#' @return Kendall's W (coefficient of concordance) 
+#' @details 
+#' @examples
+#' 
+#' library("PlackettLuce")
+#' G <- rank_tricot(data = beans,
+#'                  items = c(1:3),
+#'                  input = c(4:5),
+#'                  additional.rank = beans[c(6:8)],
+#'                  group = TRUE)
+#'                  
+#' beans <- cbind(G, beans)
+#' 
+#' beans_plt <- pltree(G ~ maxTN,
+#'                     data = beans)
+#'                    
+#' 
+#' kendallW(beans_plt)
+
+#' @export
+kendallW <- function(x, ...) {
+  
+  UseMethod("kendallW")
+  
+}
+
+#' @rdname kendallW 
+#' @export
+kendallW.default <- function(x, y, ...) {
+  
+  pred_ranks <- x
+  
+  obs_ranks <- y
+  
+  kw <- mean(unlist(lapply(X = seq_len(nrow(pred_ranks)),
+                           FUN = function(X){
+                             DescTools::KendallW(data.frame("pred" = pred_ranks[X, ],
+                                                            "obs" = obs_ranks_mtx[X, ]),
+                                                 correct = TRUE,
+                                                 test = TRUE)$estimate
+                           })))
+  
+  return(kw)
+  
+}
+
+
+#' @rdname kendallW
+#' @method kendallW pltree
+#' @export
+kendallW.pltree <- function(x, newdata = NULL) {
+  
+  if(class(x)[1] != "pltree"){
+    stop("x is not an object of class pltree")
+  }
+  
+   obs_ranks <- x[[1]]$data[[1]]
+  
+   obs_ranks_mtx <- obs_ranks[1:length(obs_ranks), , as.grouped_rankings = FALSE]
+   
+  
+  if(is.null(newdata)){
+    
+    
+    pred_ranks <- predict(x, type = "rank")
+    
+  }
+   
+   if(!is.null(newdata)){
+     
+     
+     pred_ranks <- predict(x, newdata = newdata, type = "rank")
+     
+   }
+   
+   kendallW(pred_ranks, obs_ranks)
+  
+}
+
+
